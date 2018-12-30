046601e03bf975b04e1b8e2deb81cfd1b9c18803
Rob Broadwell <hello@robbroadwell.com>
2018-12-30 11:27:42 -0700

    Initial Commit

---
+++ IBDesignable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBDesignable
+//
+//  Created by Noah on 12/30/18.
+//  Copyright © 2018 Rob Broadwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
